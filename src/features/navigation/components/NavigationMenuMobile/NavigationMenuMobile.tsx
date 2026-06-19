5d51e8521639b5cac1b3be07da71abe74562b1c3
JonasLillebo <lillebohaugen@gmail.com>
2026-06-19 19:39:13 +0200

    refactor: clean up imports and formatting in NavigationMenuMobile and routeTree.gen files

--- src/features/navigation/components/NavigationMenuMobile/NavigationMenuMobile.tsx
+++ src/features/navigation/components/NavigationMenuMobile/NavigationMenuMobile.tsx
@@ -1,12 +1,10 @@
 import { Animate } from "@/components/Animate/Animate"
 import { NavigationMenu } from "@/components/NavigationMenu/NavigationMenu"
 import { menuOpenAtom } from "@/features/navigation/navigationAtoms"
-import { Link } from "@tanstack/react-router"
 import { useAtom } from "jotai"
-import { ExternalLink } from "lucide-react"
 import type { FC } from "react"
-import "./NavigationMenuMobile.css"
 import { CommonLinks } from "@/features/navigation/components/CommonLinks/CommonLinks"
+import "./NavigationMenuMobile.css"
 
 export const NavigationMenuMobile: FC = () => {
   const [open, setOpen] = useAtom(menuOpenAtom)
